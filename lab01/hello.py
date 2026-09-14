c279079d08c7d1ea9441d68ea59455d1c3dc92a7
jamiespillane365 <c00323560@setu.ie>
2026-09-14 10:59:32 +0000

    Complete Lab 1

---
+++ lab01/hello.py
@@ -0,0 +1,7 @@
+name = input("Whats is your name? ")
+course = input("What course are you studying? ")
+area = input("What area of cybersecurity interests you? ")
+
+print("Hello " + name )
+print("You are studying " + course )
+print("You are interested in " + area )
---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+studentId = input("What is your student id? ")
+courseYear = input("What year are you in? ")
+
+print("Name: " + name )
+print("Student Number: " + studentId)
+print("Year: " + courseYear)
